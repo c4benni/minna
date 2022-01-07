e33ab8fc1c8f5806a1ece69d64b33e4a91b3a13e
Chidi Benedict <c4benni@gmail.com>
2022-01-07 09:29:55 +0100

    updated gh-pages

--- docs/_nuxt/static/1641543297/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblank\u002Fpayment-page","\u002Fpayments","\u002Fcards","\u002Fblank\u002Fcards-page","\u002Fblank\u002Fairtime-page","\u002Fblank\u002Ftransaction-history","\u002Fsupport","\u002Fblank\u002Fsend-money-page","\u002Fprofile","\u002Fblank\u002Ftransaction-history-page"]})
---
+++ docs/_nuxt/static/1641544119/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fpayments","\u002Fblank\u002Fcards-page","\u002Fprofile","\u002Fblank\u002Ftransaction-history","\u002Fsupport","\u002Fcards","\u002Fblank\u002Fpayment-page","\u002Fblank\u002Fairtime-page","\u002Fblank\u002Fsend-money-page","\u002Fblank\u002Ftransaction-history-page"]})
